a37b40fba1aa994d92300a657cf5439f70a67f91
Guilherme Perondi <guilhermeperondi09@gmail.com>
2025-05-19 10:51:08 -0300

    Correçao CAPTCHA e pagina de login

--- Data/obj/Debug/net9.0/Data.AssemblyInfo.cs
+++ Data/obj/Debug/net9.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e05833bc3871020708412f6598f8d2c292231000")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b0ef2700005098ec4fe6560585d7afb5d38990")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+864cfbab353b62d5445a4e036fabdf253186454f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b0ef2700005098ec4fe6560585d7afb5d38990")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
